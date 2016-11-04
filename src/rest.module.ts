ad83f9c4e697079ee36c8e0692ace0aa41232344
Hugh Boylan <hugh.boylan@nara.gov>
2016-11-04 09:37:13 -0400

    chore(release): 0.0.2

--- src/rest.module.ts
+++ src/rest.module.ts
@@ -1,6 +1,22 @@
 import {NgModule} from '@angular/core';
 import {HttpModule} from '@angular/http';
 import {RESTClient} from './rest.service';
+export {
+  RESTClient,
+  BaseUrl,
+  DefaultHeaders,
+  GET,
+  POST,
+  PUT,
+  DELETE,
+  HEAD,
+  Headers,
+  Path,
+  Body,
+  Query,
+  Produces,
+  MediaType
+} from './rest.service';
 
 @NgModule({
   declarations: [],
